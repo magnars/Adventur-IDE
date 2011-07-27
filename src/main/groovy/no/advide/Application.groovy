0a32d8b9d6396cfa2298552478815479e81e1ec8
Magnar Sveen <magnars@gmail.com>
2011-07-27 23:47:50 +0200

    - Flyttet oppsett av UI inn i AppFrame

--- src/main/groovy/no/advide/Application.groovy
+++ src/main/groovy/no/advide/Application.groovy
@@ -1,17 +1,8 @@
 package no.advide
 
-import groovy.swing.SwingBuilder
-import java.awt.BorderLayout
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Toolkit
-import javax.swing.BorderFactory
-import javax.swing.BoxLayout
-import javax.swing.WindowConstants
 import no.advide.ui.AppFrame
 import no.advide.ui.EditorPanel
 import no.advide.ui.KeyInterpreter
-import no.advide.ui.BackgroundPanel
 
 class Application {
 
@@ -24,8 +15,9 @@ class Application {
   ]
 
   static main(args) {
-    def appFrame = new AppFrame()
     def editorPanel = new EditorPanel()
+    def appFrame = new AppFrame(editorPanel)
+
     def editor = new Editor(lines: lines, cursor: new Cursor(x: 0, y: 0))
 
     editorPanel.textLayout = new TextLayout(lines: editor.lines, cursor: editor.cursor)
@@ -39,21 +31,8 @@ class Application {
     keys.addListener editor
     keys.onAction "cmd+enter", { appFrame.toggleFullScreen() }
 
-
-    new SwingBuilder().edt {
-      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-      frame(appFrame, title: 'ide-adv', size: [1120, screenSize.height.intValue() - 100], location: [50, 30],  show: true, defaultCloseOperation: WindowConstants.DISPOSE_ON_CLOSE) {
-        borderLayout()
-        panel(new BackgroundPanel(), constraints: BorderLayout.CENTER, border: BorderFactory.createEmptyBorder(5, 10, 5, 10)) {
-          boxLayout(axis:BoxLayout.Y_AXIS)
-          panel(maximumSize: [1000, 150], minimumSize: [0, 150], opaque: false) {
-            label(text: "Master - Rom 0", foreground: new Color(150, 150, 140))
-          }
-          panel(editorPanel, maximumSize: [1000, 2000], focusable: true, opaque: false)
-        }
-      }
-    }
-
+    appFrame.setHeaderText("Master - Rom 0")
+    appFrame.show()
   }
 
 }
--- src/main/groovy/no/advide/ui/AppFrame.groovy
+++ src/main/groovy/no/advide/ui/AppFrame.groovy
@@ -1,22 +1,56 @@
 package no.advide.ui
 
-import java.awt.GraphicsEnvironment
+import groovy.swing.SwingBuilder
+import javax.swing.BorderFactory
+import javax.swing.BoxLayout
 import javax.swing.JFrame
+import javax.swing.WindowConstants
+import java.awt.*
+import javax.swing.JLabel
 
 class AppFrame extends JFrame {
 
-  def device = GraphicsEnvironment.localGraphicsEnvironment.defaultScreenDevice
+  EditorPanel editorPanel
+  GraphicsDevice device
+  Dimension screenSize
+  JLabel header
+
+  AppFrame(edPanel) {
+    editorPanel = edPanel
+    device = GraphicsEnvironment.localGraphicsEnvironment.defaultScreenDevice
+    screenSize = Toolkit.getDefaultToolkit().getScreenSize()
+    setupUI()
+  }
 
   void toggleFullScreen() {
     if (device.fullScreenSupported) {
       this.dispose()
-      if (isFullScreen()) {
-        exitFullScreen()
-      } else {
-        enterFullScreen()
-      }
+      if (isFullScreen()) { exitFullScreen() } else { enterFullScreen() }
       this.show()
     }
+  }
+
+  void setHeaderText(text) {
+    header.text = text 
+  }
+
+  private void setupUI() {
+    new SwingBuilder().edt {
+      frame(this, title: 'ide-adv', size: frameSize(), location: [50, 30], show: false, defaultCloseOperation: WindowConstants.DISPOSE_ON_CLOSE) {
+        borderLayout()
+        panel(new BackgroundPanel(), constraints: BorderLayout.CENTER, border: BorderFactory.createEmptyBorder(5, 10, 5, 10)) {
+          boxLayout(axis: BoxLayout.Y_AXIS)
+          panel(maximumSize: [1000, 150], minimumSize: [0, 150], opaque: false) {
+            header = label(foreground: new Color(150, 150, 140))
+          }
+          panel(editorPanel, maximumSize: [1000, 2000], focusable: true, opaque: false)
+        }
+      }
+    }
+  }
+
+  private ArrayList<Integer> frameSize() {
+    return [1120, screenSize.height.intValue() - 100]
   }
 
   private def enterFullScreen() {
