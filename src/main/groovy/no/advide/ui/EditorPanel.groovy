389609a67e1130de6f9bf39d8a913dd9be4ed2aa
Magnar Sveen <magnars@gmail.com>
2011-07-28 07:53:35 +0200

    - Refactored rendering into own class

--- src/main/groovy/no/advide/ui/EditorPanel.groovy
+++ src/main/groovy/no/advide/ui/EditorPanel.groovy
@@ -7,9 +7,11 @@ import java.awt.*
 class EditorPanel extends JPanel {
 
   def textLayout
+  def defaultFont
 
   EditorPanel() {
     setFocusTraversalKeysEnabled(false)
+    defaultFont = new Font("Monaco", Font.PLAIN, 20)
   }
 
   @Override
@@ -18,27 +20,8 @@ class EditorPanel extends JPanel {
     Graphics2D g = (Graphics2D) graphics
     g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
     g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-
-    g.setFont(new Font("Monaco", Font.PLAIN, 20))
-
-    def y = 0
-    def fm = g.getFontMetrics()
-    def ascent = fm.getAscent()
-    def fh = ascent + fm.getDescent()
-
-    Cursor cursor = textLayout.cursor
-
-    textLayout.lines.eachWithIndex { String line, i ->
-      if (cursor.y == i) {
-        g.setColor(new Color(255, 255, 255, 50))
-        g.fillRect 0, y, getWidth(), fh - 1
-        g.setColor(Color.black)
-      }
-      g.drawString(line, 20, y + ascent)
-      if (cursor.y == i) { g.drawRect 20 + fm.stringWidth(line.substring(0, cursor.x)), y, 1, fh - 1 }
-
-      y = y + fh
-    }
+    g.setFont(defaultFont)
+    new Rendering(textLayout, getWidth(), g).render()
   }
 
 }
---
+++ src/main/groovy/no/advide/ui/Rendering.groovy
@@ -0,0 +1,70 @@
+package no.advide.ui
+
+import java.awt.Color
+import java.awt.Graphics2D
+
+import no.advide.TextLayout
+import java.awt.FontMetrics
+import no.advide.Cursor
+
+class Rendering {
+
+  private static final int LEFT_PADDING = 20
+  private static final Color SOFT_HIGHLIGHT = new Color(255, 255, 255, 50)
+
+  TextLayout textLayout
+  int componentWidth
+  Cursor cursor
+  Graphics2D g
+  FontMetrics fontMetrics
+  int ascent
+  int fontHeight
+  int y
+
+  Rendering(layout, width, graphics) {
+    g = graphics
+    textLayout = layout
+    componentWidth = width
+    y = 0
+    fontMetrics = g.getFontMetrics()
+    ascent = fontMetrics.getAscent()
+    fontHeight = ascent + fontMetrics.getDescent()
+    cursor = textLayout.cursor
+  }
+
+  def render() {
+    textLayout.lines.eachWithIndex { String line, i ->
+      renderLine(i, line)
+      y = y + fontHeight
+    }
+  }
+
+  private def renderLine(i, String line) {
+    if (atCursorLine(i)) renderCursorIndicatorBar()
+    renderText(line)
+    if (atCursorLine(i)) renderCursor(line)
+  }
+
+  private def renderCursor(String line) {
+    g.drawRect LEFT_PADDING + cursorX(line), y, 1, fontHeight - 1
+  }
+
+  private int cursorX(String line) {
+    return fontMetrics.stringWidth(line.substring(0, cursor.x))
+  }
+
+  private def renderText(String line) {
+    g.drawString(line, LEFT_PADDING, y + ascent)
+  }
+
+  private def renderCursorIndicatorBar() {
+    g.setColor(SOFT_HIGHLIGHT)
+    g.fillRect 0, y, componentWidth, fontHeight - 1
+    g.setColor(Color.black)
+  }
+
+  private boolean atCursorLine(i) {
+    cursor.y == i
+  }
+
+}
